9c4510485c8641935ab52de0bac65e698127aef6
Graeme Cameron <gcameron4981@gmail.com>
2017-03-17 17:44:25 +0000

    day 1 done

---
+++ app/src/main/java/example/codeclan/com/game_keeper/MainActivity.java
@@ -0,0 +1,14 @@
+package example.codeclan.com.game_keeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+/**
+ * Created by user on 17/03/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+ImageView  map = (ImageView) findViewById(R.id.mapView);
+    map.setImageResource(R.drawable.mapfile);
+
+}
